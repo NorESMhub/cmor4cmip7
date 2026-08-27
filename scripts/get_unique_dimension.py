76b68064a02741129ebd6d8b28a2fecb787edb56
He Yanchun <yanchun.ho@gmail.com>
2026-08-27 10:02:03 +0200

    update python script for manipulating json files

--- scripts/get_unique_dimension.py
+++ scripts/get_unique_dimension.py
@@ -3,10 +3,32 @@
 import json
 
 fname = '../tables/CMIP7_ocean.json'
+#fname = '../tables/CMIP7_seaIce.json'
+#fname = '../tables/CMIP7_ocnBgchem.json'
+#fname = '../tables/CMIP7_landIce.json'
 
 with open(fname,'r') as file:
     data = json.load(file)
-    unique_dimensions = {item for key in data['variable_entry']
+
+    coord_full = {key.split("_")[1] for key in data['variable_entry']}
+
+    dimensions_full = {tuple(data['variable_entry'][key]['dimensions']) for key in data['variable_entry']}
+
+    dimensions_single = {item for key in data['variable_entry']
                         for item in data['variable_entry'][key]['dimensions']}
-print(unique_dimensions)
+
+print('full coord')
+for item in coord_full:
+    print(item)
+
+print("\n")
+
+print('full dimensions')
+for item in dimensions_full:
+    print(item)
+
+print('')
+
+print('sigle dimensions')
+print(dimensions_single)
 
--- scripts/sort_by_keys.py
+++ scripts/sort_by_keys.py
@@ -2,16 +2,43 @@
 
 import json
 
-fname = '../recipes/template/variable_mapping_NorESM3_to_CMIP7.json'
+# read in existing entries
+fname = '../recipes/template/mapping.json'
 with open(fname,'r') as file:
     data_dict = json.load(file)
 
 sorted_data_dict = dict()
-sorted_data_dict['Header'] = data_dict['Header']
+sorted_data_dict = data_dict
+#print(sorted_data_dict['variable_entry'].keys())
+for key in data_dict['variable_entry'].keys():
+    print(key)
+
+exit
+#sorted_data_dict['Header'] = data_dict['Header']
+
+# import and add more entries
+dreq_file='cmip7_dreq_noresm_ocn.txt'
+dreq_file='cmip7_dreq_noresm_ocnBgc.txt'
+with open(dreq_file, 'r') as ifile:
+    for n, line in enumerate(ifile,1):
+        line = line.strip()
+        if not line:
+            continue
+
+        parts = line.split()
+        if len(parts) < 2:
+            print(f"Warning: line {line_num} has fewer than 2 columns, skipping")
+            continue
+
+        key=parts[0]
+        original_name = " ".join(parts[1:])
+        sorted_data_dict['variable_entry'][key] = {"original_name": original_name, "sources": {"var": 1.0},"postprocs": {"post": True},"history": "", "comment": ""}
+        print(f"key:{key}")
+
 sorted_data_dict['variable_entry'] = {k: v for k, v in sorted(data_dict['variable_entry'].items(), key=lambda item: item[0])}
 
-print(sorted_data_dict)
-#
-with open(fname, 'w') as f:
-    json.dump(sorted_data_dict, f, indent=4) #
+# write to new json file
+fname2 = '../recipes/template/mapping_sorted.json'
+with open(fname2, 'w') as f:
+    json.dump(sorted_data_dict, f, indent=4)
 

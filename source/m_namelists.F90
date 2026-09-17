cf922c26eef4e66bc1b93aa0ef7c5b1242c8d874
He Yanchun <yanchun.ho@gmail.com>
2026-09-17 13:34:11 +0200

    move namelist of mapfile to the variables.nml

--- source/m_namelists.F90
+++ source/m_namelists.F90
@@ -16,9 +16,9 @@ module m_namelists
   integer, parameter :: rowmax = 200, slenmax = 1024
 
   ! System namelist
-  character(len=slenmax), save  :: ibasedir, obasedir, tabledir, griddata, mapfile
+  character(len=slenmax), save  :: ibasedir, obasedir, tabledir, griddata
   logical, save                 :: createsubdirs, forcefilescan, verbose
-  namelist /sys/ ibasedir, obasedir, tabledir, griddata, mapfile, createsubdirs, &
+  namelist /sys/ ibasedir, obasedir, tabledir, griddata, createsubdirs, &
     forcefilescan, verbose
 
   ! Model namelist
@@ -80,13 +80,13 @@ module m_namelists
     forcing_index, physics_index, realization_index, initialization_index
 
   ! Variables
-  character(len=slenmax), save  :: pomon
+  character(len=slenmax), save  :: pomon, mapfile
   integer, save                 :: n_datasets
   character(len=slenmax), dimension(rowmax), save :: compound_names, branded_names, &
                                                      realms, frequencies, regions
   character(len=slenmax) :: realm, frequency, region_label
 
-  namelist /variables/ compound_names
+  namelist /variables/ mapfile, compound_names
 
   ! Misc
   integer :: istatus, funit
